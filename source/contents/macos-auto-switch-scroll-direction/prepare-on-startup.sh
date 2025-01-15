3898c239460187f33e34641ae1ec53ef15b57bc7
Alpha Hinex <AlphaHinex@gmail.com>
2025-01-15 16:30:51 +0800

    Update prepare script

--- source/contents/macos-auto-switch-scroll-direction/prepare-on-startup.sh
+++ source/contents/macos-auto-switch-scroll-direction/prepare-on-startup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Check if the network cable is connected (en8 is depended)
-if ifconfig en8 | grep -q "status: active"; then
-    network_cable_active=true
+#if ifconfig en8 | grep -q "status: active"; then
+#    in_office_env=true
+#else
+#    in_office_env=false
+#fi
+
+# Check if outer display is connected
+if system_profiler SPDisplaysDataType | grep TG34C3U; then
+    in_office_env=true
 else
-    network_cable_active=false
+    in_office_env=false
 fi
 
-if $network_cable_active; then
+if $in_office_env; then
     # Disable Wi-Fi
-    networksetup -setairportpower en0 off
+    # networksetup -setairportpower en0 off
     # Open apps
     open -a "Proxifier 2"
     osascript -e 'tell application "Proxifier 2" to set visible of every window to false'
     open -a "Lark"
-    open -a "WeChat"
+    # open -a "WeChat"
     # TODO Login WeChat
     open -a "印象笔记"
     open -a "Google Chrome"
